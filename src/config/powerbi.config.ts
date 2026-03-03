e258708fdeaf6bc8ff550282c084b7f84663944e
Lucy Kien <lkien@fieldsync.io>
2026-03-03 12:32:50 -0700

    Added the dashboards for pm and technicians

--- src/config/powerbi.config.ts
+++ src/config/powerbi.config.ts
@@ -11,19 +11,19 @@ export interface PBIConfig {
 
 export const PBI_CONFIGS: Record<'admin' | 'pm' | 'technician', PBIConfig> = {
   admin: {
-    iframeUrl: 'https://fieldsyncservices.sharepoint.com/sites/FieldSyncOperations/_layouts/15/embed.aspx?UniqueId=aaa4d74a-57a6-4496-a03f-e94c0e6995c5',      // paste your "Publish to web" src URL here
+    iframeUrl: 'https://app.powerbi.com/onedrive/open?pbi_source=ODSPViewer&driveId=b!hTeS7dxrnEmCaX5mSyx1hpmsDuLywdlOlL_3cYtie1U0BSRs4Z4DSa1HwjKb8qGu&itemId=01235L3J2K26SKVJSXSZCKAP7JJQHGTFOF',      // paste your "Publish to web" src URL here
     reportId: '',       // Azure AD: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
     embedUrl: '',       // Azure AD: 'https://app.powerbi.com/reportEmbed?reportId=...'
     accessToken: '',    // Azure AD: embed token from backend
   },
   pm: {
-    iframeUrl: '',
+    iframeUrl: 'https://app.powerbi.com/reportEmbed?reportId=0ecbf9f8-dfdc-456a-a537-367a5d0585da&autoAuth=true&ctid=f7315aa7-49bd-4b20-9699-d1480b9b30b5',
     reportId: '',
     embedUrl: '',
     accessToken: '',
   },
   technician: {
-    iframeUrl: '',
+    iframeUrl: 'https://app.powerbi.com/reportEmbed?reportId=aff3106d-cae9-4e1c-9310-2e642afa66f0&autoAuth=true&ctid=f7315aa7-49bd-4b20-9699-d1480b9b30b5',
     reportId: '',
     embedUrl: '',
     accessToken: '',
